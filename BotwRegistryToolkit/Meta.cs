bfee6fc95de7b07c57bb9407124a8ddc31bc3061
ArchLeaders <archparliament@gmail.com>
2022-12-22 23:00:43 -0800

    Create app meta data

---
+++ BotwRegistryToolkit/Meta.cs
@@ -0,0 +1,10 @@
+﻿namespace BotwRegistryToolkit
+{
+    public static class Meta
+    {
+        public static string Name { get; set; } = "Botw Registry Toolkit";
+        public static string? Version { get; set; } = typeof(Meta).Assembly.GetName().Version?.ToString(3);
+        public static string Title { get; set; } = $"{Name} - v{Version}";
+        public static string BaseUrl { get; } = "https://raw.githubusercontent.com/ArchLeaders/BotwRegistryToolkit/master";
+    }
+}
