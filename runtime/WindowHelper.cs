2f4b482d9d612c804a0b6621c89edfd49320c39c
ArchLeaders <archparliament@gmail.com>
2022-12-23 14:30:33 -0800

    Impl console window interop

---
+++ runtime/WindowHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BotwRegistryToolkit.Runtime
+{
+    public enum WindowMode : int { Hide = 0, Show = 5 }
+    public static partial class WindowHelper
+    {
+        [LibraryImport("kernel32.dll")]
+        private static partial IntPtr GetConsoleWindow();
+
+        [LibraryImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private static readonly IntPtr Handle = GetConsoleWindow();
+        public static void SetWindowMode(WindowMode mode)
+        {
+            ShowWindow(Handle, (int)mode);
+        }
+    }
+}
