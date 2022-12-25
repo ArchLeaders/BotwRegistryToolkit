69c4e3228d9ac5ae673b24f321b268a743218f2d
ArchLeaders <archparliament@gmail.com>
2022-12-24 16:01:06 -0800

    Load runtime during application setup

--- src/Models/SettingsModel.cs
+++ src/Models/SettingsModel.cs
@@ -1,7 +1,7 @@
 ﻿#pragma warning disable CA1822 // Mark members as static
 
 global using static BotwRegistryToolkit.Models.SettingsModel;
-
+using Avalonia.Generics.Dialogs;
 using Avalonia.SettingsFactory.Core;
 using System.Runtime.InteropServices;
 using System.Text.Json;
@@ -17,9 +17,9 @@ namespace BotwRegistryToolkit.Models
 
         [JsonIgnore]
         public string DataFolder
-            => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"{GetFolderPath(SpecialFolder.LocalApplicationData)}/{nameof(BotwRegistryToolkit)}" : $"{GetFolderPath(SpecialFolder.ApplicationData)}/{nameof(BotwRegistryToolkit)}";
+            => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"{GetFolderPath(SpecialFolder.LocalApplicationData)}\\{nameof(BotwRegistryToolkit)}" : $"{GetFolderPath(SpecialFolder.ApplicationData)}/{nameof(BotwRegistryToolkit)}";
 
-        public void Load()
+        public async void Load()
         {
             if (File.Exists(Path.Combine(DataFolder, "Config.json"))) {
                 using FileStream fs = File.OpenRead(Path.Combine(DataFolder, "Config.json"));
@@ -27,6 +27,36 @@ namespace BotwRegistryToolkit.Models
             }
             else {
                 Config = (SettingsModel)Save();
+            }
+
+            // Collect runtime
+            if (!File.Exists($"{DataFolder}\\Runtime.exe")) {
+                if (File.Exists(".\\Runtime.exe")) {
+                    File.Copy(".\\Runtime.exe", $"{DataFolder}\\Runtime.exe");
+        }
+                else {
+                    try {
+                        using HttpClient client = new();
+                        client.DefaultRequestHeaders.Add("user-agent", $"{nameof(BotwRegistryToolkit).ToLower()}");
+
+                        string request = $"https://api.github.com/repos/ArchLeaders/{nameof(BotwRegistryToolkit)}/releases";
+                        List<Dictionary<string, JsonElement>> releases = JsonSerializer.Deserialize<List<Dictionary<string, JsonElement>>>(
+                            await client.GetStringAsync(request)) ?? throw new Exception($"Could not parse GitHub release info from '{request}'");
+
+                        string download = releases[0]["assets"][0].Deserialize<Dictionary<string, JsonElement>>()?["browser_download_url"].ToString()
+                            ?? throw new Exception($"Invalid download url from '{request}# [0] > assets > [0]'");
+
+                        File.WriteAllBytes($"{DataFolder}\\Runtime.exe", await client.GetByteArrayAsync(download));
+                    }
+                    catch {
+                        await MessageBox.ShowDialog("""
+                            Failed to retrive the runtime from the server, please download it manually and place it next to the main executable.
+
+                            [https://github.com/ArchLeaders/BotwRegistryToolkit](https://github.com/ArchLeaders/BotwRegistryToolkit/releases/latest)
+                            """, "Error", formatting: Formatting.Markdown);
+                        Exit(1);
+                    }
+                }
             }
         }
 
